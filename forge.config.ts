372b562d204c10f5f1ad265ca67b2777cea87589
Jason Scheirer <jscheirer@makenotion.com>
2025-06-13 15:02:34 -0700

    WIP

--- forge.config.ts
+++ forge.config.ts
@@ -1,64 +1,66 @@
-import { FuseV1Options, FuseVersion } from "@electron/fuses";
-import { MakerZIP } from "@electron-forge/maker-zip";
-import { AutoUnpackNativesPlugin } from "@electron-forge/plugin-auto-unpack-natives";
-import { FusesPlugin } from "@electron-forge/plugin-fuses";
-import { WebpackPlugin } from "@electron-forge/plugin-webpack";
-import type { ForgeConfig } from "@electron-forge/shared-types";
+import { FuseV1Options, FuseVersion } from "@electron/fuses"
+import { MakerZIP } from "@electron-forge/maker-zip"
+import { AutoUnpackNativesPlugin } from "@electron-forge/plugin-auto-unpack-natives"
+import { FusesPlugin } from "@electron-forge/plugin-fuses"
+import { WebpackPlugin } from "@electron-forge/plugin-webpack"
+import type { ForgeConfig } from "@electron-forge/shared-types"
 
-import { mainConfig } from "./webpack.main.config";
-import { rendererConfig } from "./webpack.renderer.config";
+import { mainConfig } from "./webpack.main.config"
+import { rendererConfig } from "./webpack.renderer.config"
+import MSIXMaker from "./msixMaker"
 
 const config: ForgeConfig = {
-	packagerConfig: {
-		asar: true,
-	},
-	rebuildConfig: {},
-	makers: [
-		{
-			name: "@felixrieseberg/electron-forge-maker-nsis",
-			config: {
-				getAppBuilderConfig: () => {
-					return {
-						artifactName: "App Setup.exe",
-						win: {
-							forceCodeSigning: false,
-						},
-					};
-				},
-			},
-		},
-		new MakerZIP({}),
-	],
-	plugins: [
-		new AutoUnpackNativesPlugin({}),
-		new WebpackPlugin({
-			mainConfig,
-			renderer: {
-				config: rendererConfig,
-				entryPoints: [
-					{
-						html: "./src/index.html",
-						js: "./src/renderer.ts",
-						name: "main_window",
-						preload: {
-							js: "./src/preload.ts",
-						},
-					},
-				],
-			},
-		}),
-		// Fuses are used to enable/disable various Electron functionality
-		// at package time, before code signing the application
-		new FusesPlugin({
-			version: FuseVersion.V1,
-			[FuseV1Options.RunAsNode]: false,
-			[FuseV1Options.EnableCookieEncryption]: true,
-			[FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
-			[FuseV1Options.EnableNodeCliInspectArguments]: false,
-			[FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
-			[FuseV1Options.OnlyLoadAppFromAsar]: true,
-		}),
-	],
-};
+  packagerConfig: {
+    asar: true,
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: "@felixrieseberg/electron-forge-maker-nsis",
+      config: {
+        getAppBuilderConfig: () => {
+          return {
+            artifactName: "App Setup.exe",
+            win: {
+              forceCodeSigning: false,
+            },
+          }
+        },
+      },
+    },
+    new MakerZIP({}),
+    new MSIXMaker({ makeAppXPath: "" }),
+  ],
+  plugins: [
+    new AutoUnpackNativesPlugin({}),
+    new WebpackPlugin({
+      mainConfig,
+      renderer: {
+        config: rendererConfig,
+        entryPoints: [
+          {
+            html: "./src/index.html",
+            js: "./src/renderer.ts",
+            name: "main_window",
+            preload: {
+              js: "./src/preload.ts",
+            },
+          },
+        ],
+      },
+    }),
+    // Fuses are used to enable/disable various Electron functionality
+    // at package time, before code signing the application
+    new FusesPlugin({
+      version: FuseVersion.V1,
+      [FuseV1Options.RunAsNode]: false,
+      [FuseV1Options.EnableCookieEncryption]: true,
+      [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
+      [FuseV1Options.EnableNodeCliInspectArguments]: false,
+      [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
+      [FuseV1Options.OnlyLoadAppFromAsar]: true,
+    }),
+  ],
+}
 
-export default config;
+export default config
--- msixMaker.ts
+++ msixMaker.ts
@@ -1,156 +1,158 @@
-import { MakerBase, type MakerOptions } from "@electron-forge/maker-base";
-import type { ForgePlatform } from "@electron-forge/shared-types";
-import { sign, type SignOptions } from "@electron/windows-sign";
-import { exec } from "child_process";
-import debug from "debug";
-import fs from "fs-extra";
-import path from "node:path";
+import { MakerBase, type MakerOptions } from "@electron-forge/maker-base"
+import type { ForgePlatform } from "@electron-forge/shared-types"
+import { sign, type SignOptions } from "@electron/windows-sign"
+import { exec } from "child_process"
+import debug from "debug"
+import fs from "fs-extra"
+import path from "node:path"
 
-const log = debug("electron-forge:maker:msix");
+const log = debug("electron-forge:maker:msix")
 
-export type MSIXCodesignOptions = Omit<SignOptions, "appDirectory">;
+export type MSIXCodesignOptions = Omit<SignOptions, "appDirectory">
 
 async function* walk(dir: string): AsyncGenerator<string> {
-	for await (const d of await fs.opendir(dir)) {
-		const entry = path.join(dir, d.name);
-		if (d.isDirectory()) {
-			yield* walk(entry);
-		} else if (d.isFile()) {
-			yield entry;
-		}
-	}
+  for await (const d of await fs.opendir(dir)) {
+    const entry = path.join(dir, d.name)
+    if (d.isDirectory()) {
+      yield* walk(entry)
+    } else if (d.isFile()) {
+      yield entry
+    }
+  }
 }
 
 const codesign = async (config: MakerMSIXConfig, outPath: string) => {
-	if (config.codesign) {
-		try {
-			if ((await fs.stat(outPath)).isDirectory()) {
-				log(`Signing directory ${outPath}`);
-				await sign({ ...config.codesign, appDirectory: outPath });
-			} else {
-				log(`Signing file ${outPath}`);
-				await sign({ ...config.codesign, files: [outPath] });
-			}
-		} catch (error) {
-			console.error(
-				"Failed to codesign using @electron/windows-sign. Check your config and the output for details!",
-				error,
-			);
-			throw error;
-		}
+  if (config.codesign) {
+    try {
+      if ((await fs.stat(outPath)).isDirectory()) {
+        log(`Signing directory ${outPath}`)
+        await sign({ ...config.codesign, appDirectory: outPath })
+      } else {
+        log(`Signing file ${outPath}`)
+        await sign({ ...config.codesign, files: [outPath] })
+      }
+    } catch (error) {
+      console.error(
+        "Failed to codesign using @electron/windows-sign. Check your config and the output for details!",
+        error
+      )
+      throw error
+    }
 
-		// Setup signing. If these variables are set, app-builder-lib will actually
-		// codesign.
-		if (!process.env.CSC_LINK && config.codesign.certificateFile) {
-			log(`Setting process.env.CSC_LINK to ${config.codesign.certificateFile}`);
-			process.env.CSC_LINK = config.codesign.certificateFile;
-		}
+    // Setup signing. If these variables are set, app-builder-lib will actually
+    // codesign.
+    if (!process.env.CSC_LINK && config.codesign.certificateFile) {
+      log(`Setting process.env.CSC_LINK to ${config.codesign.certificateFile}`)
+      process.env.CSC_LINK = config.codesign.certificateFile
+    }
 
-		if (!process.env.CSC_KEY_PASSWORD && config.codesign.certificatePassword) {
-			log("Setting process.env.CSC_KEY_PASSWORD to the passed password");
-			process.env.CSC_KEY_PASSWORD = config.codesign.certificatePassword;
-		}
-	} else {
-		log("Skipping code signing, if you need it set 'config.codesign'");
-	}
-};
+    if (!process.env.CSC_KEY_PASSWORD && config.codesign.certificatePassword) {
+      log("Setting process.env.CSC_KEY_PASSWORD to the passed password")
+      process.env.CSC_KEY_PASSWORD = config.codesign.certificatePassword
+    }
+  } else {
+    log("Skipping code signing, if you need it set 'config.codesign'")
+  }
+}
 
 export type MakerMSIXConfig = {
-	makeAppXPath?: string;
-	codesign?: MSIXCodesignOptions;
-};
+  makeAppXPath?: string
+  codesign?: MSIXCodesignOptions
+}
 
 const makeFileMappingAndFindMainExe = async (
-	options: MakerOptions,
-	mappingFileName: string,
-	rootPath: string,
+  options: MakerOptions,
+  mappingFileName: string,
+  rootPath: string
 ): Promise<string | undefined> => {
-	let mainExe: string | undefined;
+  let mainExe: string | undefined
 
-	log(`Writing file mapping to ${mappingFileName}`);
-	let content = "[Files]\n";
-	for await (const fileName of await walk(rootPath)) {
-		const relativeFileName =
-			`VFS\\UserProgramFiles\\${options.appName}\\` +
-			fileName.substring(rootPath.length).replace(/^[\\/]+/, "");
-		// Did we fine the executable?
-		if (relativeFileName.toLowerCase().endsWith(".exe") && !mainExe) {
-			mainExe = relativeFileName;
-		}
-		content += `"${fileName}" "${relativeFileName}"\n`;
-	}
+  log(`Writing file mapping to ${mappingFileName}`)
+  let content = "[Files]\n"
+  for await (const fileName of await walk(rootPath)) {
+    const relativeFileName =
+      `VFS\\UserProgramFiles\\${options.appName}\\` +
+      fileName.substring(rootPath.length).replace(/^[\\/]+/, "")
+    // Did we fine the executable?
+    if (relativeFileName.toLowerCase().endsWith(".exe") && !mainExe) {
+      mainExe = relativeFileName
+    }
+    content += `"${fileName}" "${relativeFileName}"\n`
+  }
 
-	await fs.writeFile(mappingFileName, content);
+  await fs.writeFile(mappingFileName, content)
 
-	return mainExe;
-};
+  return mainExe
+}
 
 const makeAppManifest = async (manifestFilename: string) => {
-	log(`Writing app manifest to ${manifestFilename}`);
-	const content = "";
+  log(`Writing app manifest to ${manifestFilename}`)
+  const content = ""
 
-	await fs.writeFile(manifestFilename, content);
-};
+  await fs.writeFile(manifestFilename, content)
+}
 
 const makeMSIX = async (
-	appManifestPath: string,
-	fileMappingPath: string,
-	outMSIX: string,
-	config: MakerMSIXConfig,
+  appManifestPath: string,
+  fileMappingPath: string,
+  outMSIX: string,
+  config: MakerMSIXConfig
 ) => {
-	const makeAppXPath = config.makeAppXPath ?? "makeappx.exe";
-	const commandLine = `"${makeAppXPath}" pack /m "${appManifestPath}" /f ${fileMappingPath} /p "${outMSIX}"`;
-	log(`Running ${commandLine}`);
+  const makeAppXPath =
+    config.makeAppXPath ??
+    "C:\\Program Files (x86)\\Windows Kits\\10\\App Certification Kit\\makeappx.exe"
+  const commandLine = `"${makeAppXPath}" pack /m "${appManifestPath}" /f ${fileMappingPath} /p "${outMSIX}"`
+  log(`Running ${commandLine}`)
 
-	await exec(commandLine);
-	await codesign(config, outMSIX);
-};
+  await exec(commandLine)
+  await codesign(config, outMSIX)
+}
 
 export default class MakerMSIX extends MakerBase<MakerMSIXConfig> {
-	name = "msix";
-	defaultPlatforms: ForgePlatform[] = ["win32"];
+  name = "msix"
+  defaultPlatforms: ForgePlatform[] = ["win32"]
 
-	isSupportedOnCurrentPlatform(): boolean {
-		return process.platform === "win32";
-	}
+  isSupportedOnCurrentPlatform(): boolean {
+    return process.platform === "win32"
+  }
 
-	async make(options: MakerOptions): Promise<string[]> {
-		// Copy out files to scratch directory for signing/packaging
-		const scratchPath = path.join(options.makeDir, `scratch/`);
-		await fs.ensureDir(scratchPath);
-		await fs.copy(options.dir, scratchPath);
-		await codesign(this.config, scratchPath);
+  async make(options: MakerOptions): Promise<string[]> {
+    // Copy out files to scratch directory for signing/packaging
+    const scratchPath = path.join(options.makeDir, `scratch/`)
+    await fs.ensureDir(scratchPath)
+    await fs.copy(options.dir, scratchPath)
+    await codesign(this.config, scratchPath)
 
-		// Make sure the build dir exists
-		const outPath = path.join(
-			options.makeDir,
-			`${options.appName}-${options.targetArch}-msix/`,
-		);
-		await fs.ensureDir(outPath);
+    // Make sure the build dir exists
+    const outPath = path.join(
+      options.makeDir,
+      `${options.appName}-${options.targetArch}-msix/`
+    )
+    await fs.ensureDir(outPath)
 
-		//
-		const fileMappingPath = path.join(outPath, "filemapping.txt");
-		const mainExe = await makeFileMappingAndFindMainExe(
-			options,
-			fileMappingPath,
-			scratchPath,
-		);
-		if (!mainExe) {
-			log("Did not find an executable!");
-			throw new Error(
-				`No executable found for package in ${options.dir}, is this a packaged electron app?`,
-			);
-		}
+    //
+    const fileMappingPath = path.join(outPath, "filemapping.txt")
+    const mainExe = await makeFileMappingAndFindMainExe(
+      options,
+      fileMappingPath,
+      scratchPath
+    )
+    if (!mainExe) {
+      log("Did not find an executable!")
+      throw new Error(
+        `No executable found for package in ${options.dir}, is this a packaged electron app?`
+      )
+    }
 
-		const appManifestPath = path.join(outPath, "appmanifest.xml");
-		await makeAppManifest(appManifestPath);
+    const appManifestPath = path.join(outPath, "appmanifest.xml")
+    await makeAppManifest(appManifestPath)
 
-		const outMSIX = path.join(
-			outPath,
-			`${options.appName} ${options.packageJSON.version} (${options.targetArch}).msix`,
-		);
-		await makeMSIX(appManifestPath, fileMappingPath, outMSIX, this.config);
+    const outMSIX = path.join(
+      outPath,
+      `${options.appName} ${options.packageJSON.version} (${options.targetArch}).msix`
+    )
+    await makeMSIX(appManifestPath, fileMappingPath, outMSIX, this.config)
 
-		return [outMSIX];
-	}
+    return [outMSIX]
+  }
 }
