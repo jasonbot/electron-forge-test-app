a531e43bb07b00609fb76e1a764014c89b90dcad
Jason Scheirer <jscheirer@makenotion.com>
2025-06-13 14:10:42 -0700

    WIP

--- msixMaker.ts
+++ msixMaker.ts
@@ -1,7 +1,14 @@
 import { MakerBase, type MakerOptions } from "@electron-forge/maker-base";
 import type { ForgePlatform } from "@electron-forge/shared-types";
+import { sign, type SignOptions } from "@electron/windows-sign";
+import { exec } from "child_process";
+import debug from "debug";
 import fs from "fs-extra";
 import path from "node:path";
+
+const log = debug("electron-forge:maker:msix");
+
+export type MSIXCodesignOptions = Omit<SignOptions, "appDirectory">;
 
 async function* walk(dir: string): AsyncGenerator<string> {
 	for await (const d of await fs.opendir(dir)) {
@@ -14,20 +21,89 @@ async function* walk(dir: string): AsyncGenerator<string> {
 	}
 }
 
-export type MakerMSIXConfig = {
-	sign?: undefined;
+const codesign = async (config: MakerMSIXConfig, outPath: string) => {
+	if (config.codesign) {
+		try {
+			if ((await fs.stat(outPath)).isDirectory()) {
+				log(`Signing directory ${outPath}`);
+				await sign({ ...config.codesign, appDirectory: outPath });
+			} else {
+				log(`Signing file ${outPath}`);
+				await sign({ ...config.codesign, files: [outPath] });
+			}
+		} catch (error) {
+			console.error(
+				"Failed to codesign using @electron/windows-sign. Check your config and the output for details!",
+				error,
+			);
+			throw error;
+		}
+
+		// Setup signing. If these variables are set, app-builder-lib will actually
+		// codesign.
+		if (!process.env.CSC_LINK && config.codesign.certificateFile) {
+			log(`Setting process.env.CSC_LINK to ${config.codesign.certificateFile}`);
+			process.env.CSC_LINK = config.codesign.certificateFile;
+		}
+
+		if (!process.env.CSC_KEY_PASSWORD && config.codesign.certificatePassword) {
+			log("Setting process.env.CSC_KEY_PASSWORD to the passed password");
+			process.env.CSC_KEY_PASSWORD = config.codesign.certificatePassword;
+		}
+	} else {
+		log("Skipping code signing, if you need it set 'config.codesign'");
+	}
 };
 
-const makeFileMapping = async (mappingFileName: string, rootPath: string) => {
+export type MakerMSIXConfig = {
+	makeAppXPath?: string;
+	codesign?: MSIXCodesignOptions;
+};
+
+const makeFileMappingAndFindMainExe = async (
+	options: MakerOptions,
+	mappingFileName: string,
+	rootPath: string,
+): Promise<string | undefined> => {
+	let mainExe: string | undefined;
+
+	log(`Writing file mapping to ${mappingFileName}`);
 	let content = "[Files]\n";
 	for await (const fileName of await walk(rootPath)) {
-		const relativeFileName = fileName
-			.substring(rootPath.length)
-			.replace(/^[\\/]+/, "");
+		const relativeFileName =
+			`VFS\\UserProgramFiles\\${options.appName}\\` +
+			fileName.substring(rootPath.length).replace(/^[\\/]+/, "");
+		// Did we fine the executable?
+		if (relativeFileName.toLowerCase().endsWith(".exe") && !mainExe) {
+			mainExe = relativeFileName;
+		}
 		content += `"${fileName}" "${relativeFileName}"\n`;
 	}
 
 	await fs.writeFile(mappingFileName, content);
+
+	return mainExe;
+};
+
+const makeAppManifest = async (manifestFilename: string) => {
+	log(`Writing app manifest to ${manifestFilename}`);
+	const content = "";
+
+	await fs.writeFile(manifestFilename, content);
+};
+
+const makeMSIX = async (
+	appManifestPath: string,
+	fileMappingPath: string,
+	outMSIX: string,
+	config: MakerMSIXConfig,
+) => {
+	const makeAppXPath = config.makeAppXPath ?? "makeappx.exe";
+	const commandLine = `"${makeAppXPath}" pack /m "${appManifestPath}" /f ${fileMappingPath} /p "${outMSIX}"`;
+	log(`Running ${commandLine}`);
+
+	await exec(commandLine);
+	await codesign(config, outMSIX);
 };
 
 export default class MakerMSIX extends MakerBase<MakerMSIXConfig> {
@@ -38,25 +114,43 @@ export default class MakerMSIX extends MakerBase<MakerMSIXConfig> {
 		return process.platform === "win32";
 	}
 
-	async make({
-		dir,
-		makeDir,
-		appName,
-		targetArch,
-	}: MakerOptions): Promise<string[]> {
+	async make(options: MakerOptions): Promise<string[]> {
 		// Copy out files to scratch directory for signing/packaging
-		const scratchPath = path.join(makeDir, `scratch/`);
+		const scratchPath = path.join(options.makeDir, `scratch/`);
 		await fs.ensureDir(scratchPath);
-		await fs.copy(dir, scratchPath);
+		await fs.copy(options.dir, scratchPath);
+		await codesign(this.config, scratchPath);
 
-		const outPath = path.join(makeDir, `${appName}-${targetArch}-msix/`);
+		// Make sure the build dir exists
+		const outPath = path.join(
+			options.makeDir,
+			`${options.appName}-${options.targetArch}-msix/`,
+		);
 		await fs.ensureDir(outPath);
 
+		//
 		const fileMappingPath = path.join(outPath, "filemapping.txt");
-		await makeFileMapping(fileMappingPath, scratchPath);
+		const mainExe = await makeFileMappingAndFindMainExe(
+			options,
+			fileMappingPath,
+			scratchPath,
+		);
+		if (!mainExe) {
+			log("Did not find an executable!");
+			throw new Error(
+				`No executable found for package in ${options.dir}, is this a packaged electron app?`,
+			);
+		}
 
-		// fs.removeSync(scratchPath);
+		const appManifestPath = path.join(outPath, "appmanifest.xml");
+		await makeAppManifest(appManifestPath);
 
-		return [];
+		const outMSIX = path.join(
+			outPath,
+			`${options.appName} ${options.packageJSON.version} (${options.targetArch}).msix`,
+		);
+		await makeMSIX(appManifestPath, fileMappingPath, outMSIX, this.config);
+
+		return [outMSIX];
 	}
 }
